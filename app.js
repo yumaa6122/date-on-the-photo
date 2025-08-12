f118517e2b971867966e399a85e8bf3ce0526e0c
sasaki.yuma <40253520+yumaa6122@users.noreply.github.com>
2025-08-12 10:19:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ fileInput.addEventListener('change', async (e) => {
   }
   if (orientation === 'landscape') {
     scaleEl.value = 0.05;   // 高さ比
-    marginXEl.value = 0.020;  // 幅の20%
+    marginXEl.value = 0.110;  // 幅の20%
     marginYEl.value = 0.055;  // 高さの10%
     spaceEl.value = 0.5;
     //scaleEl.value = 0.05; 
@@ -138,7 +138,7 @@ fileInput.addEventListener('change', async (e) => {
     //spaceEl.value = 0.5;
   } else if (orientation === 'portrait') {
     scaleEl.value = 0.03;
-    marginXEl.value = 0.020;
+    marginXEl.value = 0.110;
     marginYEl.value = 0.055;
     spaceEl.value = 0.5;
     //scaleEl.value = 0.03; 
